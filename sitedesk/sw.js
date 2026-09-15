03e4641eb2f6869ff5e0569ebc31580ca45f4057
Not Ai <iamnottaiii@gmail.com>
2026-09-15 15:51:33 -0400

    sitedesk: sw v6 cache bust

--- sitedesk/sw.js
+++ sitedesk/sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sitedesk-v5';
+var CACHE = 'sitedesk-v6';
 var CORE = ['index.html', 'styles.css', 'app.js', 'config.js', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png', 'icon.svg'];
 
 self.addEventListener('install', function(e){
